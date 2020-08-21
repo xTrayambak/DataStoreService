bb990801031407373eb1f09b12b86114f3646dff
xTrayambak <59499552+xTrayambak@users.noreply.github.com>
2020-08-21 12:51:07 +0530

    Put in ServerScriptService or Workspace.

---
+++ DataStoreService.lua
@@ -0,0 +1,23 @@
+print("DataStore Script written by xXEpic_TrayambakXx loaded successfuly.")
+local DataStore = game:GetService("DataStoreService")
+local statsStore = DataStore:GetDataStore("StatStore")
+
+game.Players.PlayerAdded:Connect(function(player)
+  statsStore:SetAsync("UserId-"..player.UserId)
+  local leaderstats = Instance.new("Folder", player)
+  leaderstats.Name = "leaderstats"
+  local coin = Instance.new("IntValue", leaderstats)
+  coin.Name = "Coins"
+  end
+-- Finished settings, now the part when the player leaves.
+
+game.Players.PlayerRemoving:Connect(function()
+local saved, notSaved = pcall(function()
+end)
+
+if saved then
+   print("Data for Player Saved Successfuly!"
+   else
+   print("There was an error in saving data.)
+   end
+end)
